da12077ac0e3aeff2d2761d288e08218385ecbd2
shafi <shafi@DESKTOP-5CJ0DJM>
2023-04-08 22:17:17 +0400

    Add project files.

---
+++ Work1/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Work1
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health {get; set; }
+        public int Mana { get; set; }
+        public string Inventory { get; set; }
+
+        // constructor
+        public Player(string name, string inventory,int health, int mana)
+        {
+            Name = name;
+            Inventory = inventory;
+            Health = health;
+            Mana = mana;
+            
+        }
+
+        public void Attack(string Name1, string Name2 )
+        {
+            Console.WriteLine(Name1 + " attack " + Name2 );
+        }
+
+        public void UseItem(string inventory, string Name)
+        {
+            Console.WriteLine(Name + " used item: " + inventory);
+        }
+    }
+}
---
+++ Work1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Shafiqa = new Player("Shafiqa", "sword", 100, 50);
+            Player Murad = new Player("Murad", "gun", 100, 70);
+            Shafiqa.Attack(Shafiqa.Name, Murad.Name);
+            Shafiqa.UseItem(Shafiqa.Inventory, Shafiqa.Name);
+            Murad.Attack(Murad.Name, Shafiqa.Name);
+            Murad.UseItem(Murad.Inventory, Murad.Name);
+
+        }
+    }
+}
